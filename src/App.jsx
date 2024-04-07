6195c0d122059f138494cfb858d5938f122a1f11
Illia <ilja.cherkasov@gmail.com>
2024-04-07 09:46:35 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>Watch Wise</h1>
+      </header>
+    </>
+  );
+};
+
+export default App;
